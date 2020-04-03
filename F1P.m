fe0b0c6f15aea73b25adbff1acf4b1f5b5594e8f
pavindercheema <62850689+pavindercheema@users.noreply.github.com>
2020-04-03 14:54:43 +0100

    Add files via upload
    Changed indices of functions with regards to the adjusted mesh geometry

--- F1P.m
+++ F1P.m
@@ -2,18 +2,18 @@ function [F1P] = F1P(N,M,P,mesh)
 % Function file which creates the Divergence of Pressure Flux matrix for
 % the N-S equation that's a function of the 'u' velocity
 
-F1P = zeros(M,N);
+F1P = zeros(N,M);
 
-for k = 1:N
+for k = 1:M
     % Left
-    F1P(1,k) = (-P(2,k) - P(N-1,k))/(2*mesh.dx);
+    F1P(k,1) = (-P(k,2) - P(k,N-1))/(2*mesh.dx);
     
     % Right
-    F1P(M,k) = (-P(2,k) - P(N-1,k))/(2*mesh.dx);
+    F1P(k,M) = (-P(k,2) - P(k,N-1))/(2*mesh.dx);
 end
 
-for i = 2:M-1
-    for j = 2:N-1
+for i = 2:N-1
+    for j = 2:M-1
         P(i,j) = (-P(i+1,j) - P(i-1,j))/(2*mesh.dx);
     end
 end
--- F2P.m
+++ F2P.m
@@ -2,18 +2,18 @@ function [F2P] = F2P(N,M,P,mesh)
 % Function file which creates the Divergence of Pressure Flux matrix for
 % the N-S equation that's a function of the 'v' velocity
 
-F2P = zeros(M,N);
+F2P = zeros(N,M);
 
 for k = 1:M
     % Top
-    F2P(k,N) = (-P(k,N-1) - P(k,N-1))/(2*mesh.dx);
+    F2P(1,k) = (-P(N-1,k) - P(N-1,k))/(2*mesh.dx);
     
     % Bottom
-    F2P(k,1) = (-P(k,2) - P(k,2))/(2*mesh.dx);
+    F2P(N,k) = (-P(2,k) - P(2,k))/(2*mesh.dx);
 end
 
-for i = 2:M-1
-    for j = 2:N-1
+for i = 2:N-1
+    for j = 2:M-1
         % Main
         F2P(i,j) = (-P(i+1,j) - P(i-1,j))/(2*mesh.dy);
     end
