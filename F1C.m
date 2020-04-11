411fff89092645669d19259360dcaf7651a0a798
manu-city <62848618+manu-city@users.noreply.github.com>
2020-04-11 11:02:03 +0100

    Update
    - Fixed boundary conditions both FP's and FC's functions
    - Streamlined code

--- F1C.m
+++ F1C.m
@@ -1,39 +1,37 @@
-function [F1C] = F1C(u,v,mesh)
+function [F1C] = F1C(N,M,u,v,mesh)
 % Function file which creates the Divergence of Convective Flux matrix for
 % the N-S equation that's a function of the 'u' velocity
-N = mesh.nx;
-M = mesh.ny;
 
 F1C = zeros(N,M);
 
 for i = 2:N-1  
     for j = 2:M-1
-        %% Top
-        F1C(1,j) = -((u(2,j)*u(2,j) - u(N-1,j)*u(N-1,j))/(2*mesh.dx)) - ((u(1,j+1)*v(1,j+1))/(2*mesh.dy));
+        % Top
+        F1C(1,j) = ((u(1,j+1)*u(1,j+1) - u(1,j-1)*u(1,j-1))/(2*mesh.dx));
          
         % Top Left Corner
-        F1C(1,1) = -((u(1,2)*v(1,2) - (-u(1,2)*-v(1,2)))/(2*mesh.dy));
+        F1C(1,1) = -((u(1,2)*u(1,2) - (u(1,M-1)*u(1,M-1)))/(2*mesh.dx));
          
         % Left
-        F1C(i,1) = -((u(i+1,1)*u(i+1,1) - u(i-1,1)*u(i-1,1))/(2*mesh.dx))-((u(i,2)*v(i,2)) - (-u(i,2)*-v(i,2)))/(2*mesh.dy);
+        F1C(i,1) = -((u(i,2)*u(i,2)) - (-u(i,M-1)*-u(i,M-1)))/(2*mesh.dx) -((u(i+1,j)*v(i+1,j)) - (-u(i-1,j)*-v(i-1,j)))/(2*mesh.dy);
         
         % Bottom Left Corner
-        F1C(N,1) = -((u(2,1)*u(2,1) - u(N-1,1)*u(N-1,1))/(2*mesh.dx)) - ((u(N,2)*v(N,2)) - (-u(N,2)*-v(N,2)))/(2*mesh.dy);
+        F1C(N,1) = -((u(i,2)*u(i,2) - u(i,M-1)*u(i,M-1))/(2*mesh.dx));
         
         %% Main Domain
-        F1C(i,j) = -((u(i+1,j)*u(i+1,j) - u(i-1,j)*u(i-1,j))/(2*mesh.dx)) - ((u(i,j+1)*v(i,j+1) - u(i,j-1)*v(i,j-1))/(2*mesh.dy));
-        
-        %% Bottom
-        F1C(N,j) = -((u(2,j)*u(2,j)) - (u(N-1,j)*u(N-1,j))/(2*mesh.dx)) - ((u(N,j+1)*v(N,j+1)) - (-u(N,j-1)*v(N,j-1)))/(2*mesh.dy);
+        F1C(i,j) = -((u(i,j+1)*u(i,j+1) - u(i,j-1)*u(i,j-1))/(2*mesh.dx)) - ((u(i+1,j)*v(i+1,j) - u(i-1,j)*v(i-1,j))/(2*mesh.dy));
+        %%
+        % Bottom
+        F1C(N,j) = -((u(N,j+1)*u(N,j+1) - u(N,j-1)*u(N,j-1))/(2*mesh.dx));
         
         % Bottom Right Corner
-        F1C(N,M) = -((-u(2,M)*u(2,M)) - (u(N-1,M)*u(N-1,M))/(2*mesh.dx)) - ((u(N,M-1)*v(N,M-1))/(2*mesh.dy));
+        F1C(N,M) = -((u(N,2)*u(N,2) - u(N,M-1)*u(N,M-1))/(2*mesh.dx));
         
         % Right
-        F1C(i,M) = -((u(i+1,M)*u(i+1,M) - u(i-1,M)*u(i-1,M))/(2*mesh.dx)) - ((u(i,M-1)*v(i,M-1))/2*mesh.dy); 
+        F1C(i,M) = -((u(i,2)*u(i,2) - u(i,M-1)*u(i,M-1))/(2*mesh.dx)) - ((u(i+1,M)*v(i+1,M) - u(i-1,M)*v(i-1,M))/2*mesh.dy); 
         
         % Top Right Corner
-        F1C(1,M) = -((u(2,M)*u(2,M) - u(N-1,M)*u(N-1,M))/(2*mesh.dx)) - ((u(1,M-1)*v(1,M-1))/(2*mesh.dy));
+        F1C(1,M) = -((u(1,2)*u(1,2) - u(1,M-1)*u(1,M-1))/(2*mesh.dx));
         
     end
 end
--- F2C.m
+++ F2C.m
@@ -1,39 +1,37 @@
-function [F2C] = F2C(u,v,mesh)
+function [F2C] = F2C(N,M,u,v,mesh)
 % Function file which creates the Divergence of Convective Flux matrix for
 % the N-S equation that's a function of the 'v' velocity
-N = mesh.nx;
-M = mesh.ny;
 
 F2C = zeros(N,M);
 
 for i = 2:N-1
     for j = 2:M-1
         %% Top
-        F2C(1,j) = -((u(2,j)*v(2,j) - (u(N-1,j)*v(N-1,j)))/(2*mesh.dx)) - ((v(1,j+1)*v(1,j+1) - v(1,j-1)*v(1,j-1))/(2*mesh.dy));
+        F2C(1,j) = - ((v(2,j)*v(2,j) - v(2,j)*v(2,j))/(2*mesh.dy));
         
         % Top Left Corner
-        F2C(1,1) = ((v(1,2)*v(1,2) - (-v(1,2)*-v(1,2)))/(2*mesh.dy));
+        F2C(1,1) = -((v(2,1)*v(2,1) - (v(2,1)*v(2,1)))/(2*mesh.dy));
         
         % Left
-        F2C(i,1) = -((u(i+1,1)*v(i+1,1) - (u(i-1,1)*v(i-1,1)))/(2*mesh.dx)) - ((v(i,2)*v(i,2))/(2*mesh.dy));
+        F2C(i,1) = -((u(i,2)*v(i,2) - (u(i,M-1)*v(i,M-1)))/(2*mesh.dx)) - (v(i+1,1)*v(i+1,1) - v(i-1,1)*v(i-1,1))/(2*mesh.dy);
         
         % Bottom Left Corner
-        F2C(N,1) = ((v(N,2)*v(N,2)) - (-v(N,2)*-v(N,2)))/(2*mesh.dy);
+        F2C(N,1) = -((v(N-1,1)*v(N-1,1)) - (v(N-1,1)*v(N-1,1)))/(2*mesh.dy);
         
         %% Main Domain
-        F2C(i,j) = ((u(i+1,j)*v(i+1,j) - u(i-1,j)*v(i-1,j))/(2*mesh.dx)) - ((v(i,j+1)*v(i,j+1) - v(i,j-1)*v(i,j-1))/(2*mesh.dy));
+        F2C(i,j) = ((u(i,j+1)*v(i,j+1) - u(i,j+1)*v(i,j+1))/(2*mesh.dx)) - ((v(i+1,j)*v(i+1,j) - v(i-1,j)*v(i-1,j))/(2*mesh.dy));
         
         %% Bottom
-        F2C(N,j) = -((u(2,j)*v(2,j)) - (u(N-1,j)*v(N-1,j)))/(2*mesh.dx) - ((v(N,j+1)*v(N,j+1) - v(N,j-1)*v(N,j-1))/(2*mesh.dy));
+        F2C(N,j) = - ((v(N-1,j)*v(N-1,j) - v(N-1,j)*v(N-1,j))/(2*mesh.dy));
         
         % Bottom Right Corner
-        F2C(N,M) = (-v(N,M-1)*v(N,M-1))/(2*mesh.dy);
+        F2C(N,M) = - ((v(N-1,M)*v(N-1,M) - v(N-1,M)*v(N-1,M))/(2*mesh.dy));
         
         % Right
-        F2C(i,M) = -(v(i,M-1)*v(i,M-1)/(2*mesh.dy));
+        F2C(i,M) = -(u(i,2)*v(i,2) - u(i,M-1)*v(i,M-1))/(2*mesh.dx) - (v(i+1,M)*v(i+1,M) - v(i-1,M)*v(i-1,M))/(2*mesh.dy);
         
         % Top Right Corner
-        F2C(1,M) = -(v(1,M-1)*v(1,M-1)/(2*mesh.dy));
+        F2C(1,M) = - ((v(2,j)*v(2,j) - v(2,j)*v(2,j))/(2*mesh.dy));
         
     end
 end
